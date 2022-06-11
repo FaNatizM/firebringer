0d757070068ce87a22469862b41a15900f699276
FaNatizM <whitelight92@mail.ru>
2022-06-11 23:16:20 +0300

    fix /tools:      firefox for html.

--- tools/md_to_html.sh
+++ tools/md_to_html.sh
@@ -6,3 +6,5 @@ g_md_file_path=$( readlink -f ${g_md_file} )
 cd $( echo "${0}" | sed 's;\(.*\)\/.*;\1;' )
 
 pandoc ${g_md_file_path} -f markdown -t html -o ../"build/print.html"
+
+firefox ../"build/print.html"
