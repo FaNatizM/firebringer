7506b444fb90f44f8120e0d5449bc82e6ae9527d
FaNatizM <whitelight92@mail.ru>
2022-06-11 21:32:58 +0300

    dev+struc /tools:      create html file from md.

---
+++ tools/md_to_html.sh
@@ -0,0 +1,8 @@
+g_md_file=${1}
+g_md_file_path=$( readlink -f ${g_md_file} )
+
+
+# Переходим в каталог скрипта
+cd $( echo "${0}" | sed 's;\(.*\)\/.*;\1;' )
+
+pandoc ${g_md_file_path} -f markdown -t html -o ../"build/print.html"
